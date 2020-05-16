c658c740eb99829f219cf97e5114489fa02e1a28
David Yang <zyang03@risd.edu>
2020-05-16 10:37:59 -0700

    Generate Faceme PNG

--- skpThumb.rb
+++ skpThumb.rb
@@ -1,6 +1,6 @@
 f=UI.select_directory
 if f
-  pngs='E:/Documents/GitHub/PWP-Lib-Crawler/Thumbnails-3D'
+  pngs='E:/Documents/GitHub/PWP-Lib-Crawler/Thumbnails-Faceme-PNG'
   dir=Dir.mkdir(pngs) unless File.exist?(pngs)
   Dir.glob("#{f}/**/*.skp").each{|skp|
     s=Sketchup.open_file(skp)
